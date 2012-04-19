4c7a8939805936a1a59830415d789b8e0555a799
androidcook <ian@darwinsys.com>
2012-04-19 16:31:40 -0400

    Include the two trivial HelloWorld examples while we're here.

---
+++ HelloEclipse/src/com/example/hello/eclipse/HelloEclipseActivity.java
@@ -0,0 +1,13 @@
+package com.example.hello.eclipse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloEclipseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
