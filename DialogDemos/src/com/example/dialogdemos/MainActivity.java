4b089fb6cf69602afd2c23502c5486beb77f847c
androidcook <ian@darwinsys.com>
2012-08-26 15:19:08 -0400

    Start baking. Old part is baked, new not. Half baked?

--- DialogDemos/src/com/example/dialogdemos/MainActivity.java
+++ DialogDemos/src/com/example/dialogdemos/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.dialogdemos;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
+	private static final int DLG_UNSAVED = 1;
+	private static final String TAG = "DialogDemos";
+	
+	interface bleah {
+		public class X {
+			public void setFoo() {}
+		}
+	}
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -20,5 +36,46 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @SuppressWarnings("deprecation")
+	public void oldWay(View v) {
+    	showDialog(DLG_UNSAVED);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	switch(id) {
+    	case DLG_UNSAVED:
+    		final AlertDialog alertDialog = new AlertDialog.Builder(this)
+    		.setTitle(R.string.unsaved_changes_title)
+    		.setMessage(R.string.unsaved_changes_message)
+    		.setPositiveButton(R.string.save_changes, new AlertDialog.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    				// saveInformation();
+    			}
+    			})
+    		.setNeutralButton(R.string.discard_changes, new AlertDialog.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    			        // finish();
+    		        }
+    			})
+    		.setNegativeButton(R.string.cancel_dialog, new AlertDialog.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    			        // cancel();
+    			}
+    			})
+    			.create();
+    		return alertDialog;
+    		default:
+    			//@SuppressWarnings("deprecation")
+    			return super.onCreateDialog(id);
+    	}
+    }
+    
+    public void newWay(View v) {
+    	Log.d(TAG, "New Way invoked");
+    	Dialog dlg = onCreateDialog(DLG_UNSAVED);
+    	dlg.show();
+    	
+    }
     
 }
