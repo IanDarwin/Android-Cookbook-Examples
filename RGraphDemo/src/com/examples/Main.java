1f00bdb168287078192b20aa9cd4f58b2d8966e6
AndroidCook <ian@darwinsys.com>
2013-05-09 13:37:07 -0400

    Reformat, consistify variable name, add @SuppressWarnings.

--- RGraphDemo/src/com/examples/Main.java
+++ RGraphDemo/src/com/examples/Main.java
@@ -1,5 +1,6 @@
 package com.examples;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
@@ -7,26 +8,24 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class Main extends Activity
-	{
+public class Main extends Activity {
 
-		/** Called when the activity is first created. */
-		@Override
-		public void onCreate(Bundle savedInstanceState)
-			{
-				super.onCreate(savedInstanceState);
-				setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-				WebView webview = (WebView) this.findViewById(R.id.webview);
-				WebSettings webSettings = webview.getSettings();
-				webSettings.setJavaScriptEnabled(true);
-				webSettings.setBuiltInZoomControls(true);
-				webview.requestFocusFromTouch();
-				webview.setWebViewClient(new WebViewClient());
-				webview.setWebChromeClient(new WebChromeClient());
+		WebView webView = (WebView) this.findViewById(R.id.webview);
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setBuiltInZoomControls(true);
+		webView.requestFocusFromTouch();
+		webView.setWebViewClient(new WebViewClient());
+		webView.setWebChromeClient(new WebChromeClient());
 
-				// Load the URL
-				webview.loadUrl("file:///android_asset/rgraphview.html");
-			}
-
+		// Load the URL
+		webView.loadUrl("file:///android_asset/rgraphview.html");
 	}
+}
