ba3e051e0e2d2ed6f99bd399a3f36628a9b6a05e
AndroidCook <ian@darwinsys.com>
2013-06-03 15:42:52 -0400

    Exceptions -> Log.e and also into a Toast.

--- SoapDemo/src/com/example/soapdemo/SoapDemoActivity.java
+++ SoapDemo/src/com/example/soapdemo/SoapDemoActivity.java
@@ -1,26 +1,24 @@
 package com.example.soapdemo;
 
-import java.io.IOException;
-
 import org.ksoap2.SoapEnvelope;
-import org.ksoap2.SoapFault;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.xmlpull.v1.XmlPullParserException;
-
 import org.ksoap2.transport.HttpTransportSE;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SoapDemoActivity extends Activity {
 	private static final String SOAP_ACTION = "http://tempuri.org/CelsiusToFahrenheit";
 	private static final String METHOD_NAME = "CelsiusToFahrenheit";
 	private static final String NAMESPACE = "http://tempuri.org/";
 	private static final String URL = "http://www.w3schools.com/webservices/tempconvert.asmx";
+	protected static final String TAG = "SoapDemo";
 	private EditText degreesC, degreesF;
 	
 	/** Called when the activity is first created. */
@@ -51,13 +49,10 @@ public class SoapDemoActivity extends Activity {
 							degreesF.setText(result.toString());					
 						}
 					});
-				} catch (SoapFault e) {
-					System.out.println("Soap Fault: " + e.getMessage());
-				} catch (IOException e) {
-					System.out.println("IOException: " + e.getMessage());
-				} catch (XmlPullParserException e) {
-					System.out.println("XML Error: " + e.getMessage());
-				}
+                } catch (Exception e) {
+                    Log.e(TAG, "Web Service Error", e);
+                    Toast.makeText(SoapDemoActivity.this, "Web Service Error: " + e, Toast.LENGTH_LONG).show();
+                }
 			}
 		}.start();
 	}
