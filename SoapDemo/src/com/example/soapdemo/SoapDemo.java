2d27281c7c929a420dae4f00116619cdd336c00d
androidcook <ian@darwinsys.com>
2013-06-03 09:11:09 -0400

    New Project: SOAP demo using ksoap2.

---
+++ SoapDemo/src/com/example/soapdemo/SoapDemo.java
@@ -0,0 +1,53 @@
+package com.example.soapdemo;
+
+import java.io.IOException;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.xmlpull.v1.XmlPullParserException;
+
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SoapDemo extends Activity {
+	private static final String SOAP_ACTION = "http://tempuri.org/CelsiusToFahrenheit";
+	private static final String METHOD_NAME = "CelsiusToFahrenheit";
+	private static final String NAMESPACE = "http://tempuri.org/";
+	private static final String URL = "http://www.w3schools.com/webservices/tempconvert.asmx";
+	private EditText degreesC, degreesF;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main_activity);
+		degreesC = findViewById(R.id.degreesC);
+		degreesF = findViewById(R.id.degreesF);
+		try {
+		SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+		request.addProperty("Celsius", degreesC.getText());
+		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+				SoapEnvelope.VER11);
+		envelope.dotNet = true;
+		envelope.setOutputSoapObject(request);
+			HttpTransportSE httpTransport = new HttpTransportSE(URL);
+			httpTransport.debug = true;
+			httpTransport.call(SOAP_ACTION, envelope);
+			SoapPrimitive result = (SoapPrimitive) envelope.getResponse();
+			degreesF.setText(result.toString());
+		} catch (SoapFault e) {
+			System.out.println("Soap Fault: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		} catch (XmlPullParserException e) {
+			System.out.println("XML Error: " + e.getMessage());
+		}
+	}
+}
