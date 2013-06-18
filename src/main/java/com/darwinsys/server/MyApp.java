6507e152ad26336d84892b25502028f1658e99a0
IanDarwin <>
2013-06-18 21:04:12 +0000

    Add JAX-RS config class (trivial).

---
+++ src/main/java/com/darwinsys/server/MyApp.java
@@ -0,0 +1,16 @@
+package com.darwinsys.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * This is for the benefit of Java EE (specifically JAX-RS), and is only (at present)
+ * used to specify the URL mapping for this app. As the JBoss sample app states, 
+ * "A class extending {@link Application} and annotated with @ApplicationPath is the 
+ * Java EE 6 "no XML" approach to activating JAX-RS."
+ * @author Ian Darwin
+ */
+@ApplicationPath("/rest")
+public class MyApp extends Application {
+	// Look ma! No code!
+}
