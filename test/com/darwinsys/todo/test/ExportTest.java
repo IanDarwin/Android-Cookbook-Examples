a76486a5a020fe2222eb1e933fb087b8b96839a3
IanDarwin <>
2013-06-22 23:44:00 +0000

    Set up test directory, add it to .classpath and pom.xml, stub first test (fail("not impl"))

---
+++ test/com/darwinsys/todo/test/ExportTest.java
@@ -0,0 +1,14 @@
+package com.darwinsys.todo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExportTest {
+
+	@Test
+	public void testExportTasks() {
+		fail("Not yet implemented");
+	}
+
+}
