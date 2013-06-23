79c741e9edbb03ab322ce587d2772cda3fb710b4
IanDarwin <>
2013-06-23 15:14:40 +0000

    Squelch incomplete test

--- test/com/darwinsys/todo/test/ExportTest.java
+++ test/com/darwinsys/todo/test/ExportTest.java
@@ -2,11 +2,12 @@ package com.darwinsys.todo.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExportTest {
 
-	@Test
+	@Test @Ignore("Code not written yet")
 	public void testExportTasks() {
 		fail("Not yet implemented");
 	}
