9cedfb1c96d8eee6100fe88a800636aa54d869ab
IanDarwin <>
2013-06-23 15:39:36 +0000

    Test and implement complete/completedDate logic in Task, as well as toString variants.

--- src/com/darwinsys/todo/model/Task.java
+++ src/com/darwinsys/todo/model/Task.java
@@ -1,8 +1,15 @@
 package com.example.myaccount.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /** One ToDo item or "task".
  * See http://todotxt.com/ and https://github.com/ginatrapani/todo.txt-cli/wiki/The-Todo.txt-Format.
@@ -11,13 +18,23 @@ import javax.persistence.*;
 @Entity
 public class Task {
 	
+	public static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	
+	private static final char SPACE = ' ';
+
+	private static final char PROJECT = '+', CONTEXT = '@';
 	long id;
 	String name;	// what to do
 	Date creationDate; // when you decided you had to do it
 	String project;		// what this task is part of
 	String context;	// where to do it
 	Date dueDate;	// when to do it by
+	boolean complete;
 	Date completedDate; // when you actually did it
+	
+	public Task() {
+		creationDate = new Date();
+	}
 
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	public long getId() {
@@ -62,19 +79,55 @@ public class Task {
 		this.dueDate = dueDate;
 	}
 	
+	public boolean isComplete() {
+		return complete;
+	}
+
+	/**
+	 * set completion to true or false, and side-effect set completion date
+	 * to today or to null, but if setting to true and client already set
+	 * completion data, leave well enough alone.
+	 * @param complete
+	 */
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+		if (complete) {
+			if (getCompletedDate() == null) {
+				completedDate = new Date();
+			}
+		} else {
+			setCompletedDate(null);
+		}
+	}
+	
 	@Temporal(TemporalType.DATE)
 	public Date getCompletedDate() {
 		return completedDate;
 	}
 	public void setCompletedDate(Date completedDate) {
 		this.completedDate = completedDate;
+		complete = completedDate != null;
 	}
 	
-	/** ToString converts to String but in todo.txt format! */
+	/** ToString converts to String but in todo.txt format! 
+	 * A fully-fleshed-out example from todotxt.com:
+	 * x 2011-03-02 2011-03-01 Review Tim's pull request +TodoTxtTouch @github 
+	 */
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
+		if (isComplete()) {
+			sb.append('x').append(SPACE).append(dateFormat.format(getCompletedDate())).append(SPACE);
+		}
+		sb.append(dateFormat.format(getCreationDate()));
+		sb.append(SPACE);
 		sb.append(name);
+		if (getProject() != null) {
+			sb.append(SPACE).append(PROJECT).append(project);
+		}
+		if (getContext() != null) {
+			sb.append(SPACE).append(CONTEXT).append(context);
+		}
 		return sb.toString();
 	}
 }
--- test/com/darwinsys/todo/test/TaskTest.java
+++ test/com/darwinsys/todo/test/TaskTest.java
@@ -2,18 +2,59 @@ package com.darwinsys.todo.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import com.example.myaccount.model.Task;
 
 public class TaskTest {
 	
+	private static final String GET_THE_LEAD_OUT = "Get the lead out";
+	private static final char SPACE = ' ';
+	private static final String PROJECT = "Plumbing";
+	private static final String CONTEXT = "Home";
 	Task t = new Task();
-
+	String today = Task.dateFormat.format(new Date());
+	
 	@Test
-	public void testToString() {
-		t.setName("Get the lead out");
-		assertEquals("toString", "Get the lead out", t.toString());
+	public void testConstructor() {
+		assertNotNull("constructed ok", t.getCreationDate());
+	}
+	
+	@Test
+	public void testSetCompletedSetsCompletionDate() {
+		t.setComplete(true);
+		assertNotNull("completion goo", t.getCompletedDate());
+	}
+	
+	@Test
+	public void testSetNotCompletedSetsCompletionDateNull() {
+		t.setComplete(true);
+		assertNotNull("completion true", t.getCompletedDate());
+		t.setComplete(false);
+		assertNull("completion false", t.getCompletedDate());
 	}
 
+	@Test
+	public void testSimpleToString() {
+		t.setName(GET_THE_LEAD_OUT);
+		assertEquals("toString", today + ' ' + GET_THE_LEAD_OUT, t.toString());
+	}
+
+	@Test
+	public void testComplexToString() {
+		t.setName(GET_THE_LEAD_OUT);
+		t.setContext(CONTEXT);
+		t.setProject(PROJECT);
+		assertEquals("toString", today + SPACE + "Get the lead out" + SPACE + "+" + PROJECT + ' ' + "@" + CONTEXT, 
+				t.toString());
+	}
+	
+	@Test
+	public void testCompleteToString() {
+		t.setName(GET_THE_LEAD_OUT);
+		t.setComplete(true);
+		assertEquals("toString", "x" + SPACE + today + SPACE + today + ' ' + GET_THE_LEAD_OUT, t.toString());
+	}
 }
