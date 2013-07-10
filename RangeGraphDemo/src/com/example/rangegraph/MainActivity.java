c2b2ed5b4f22ecb7caef6ca98ff8ce43a869cb7a
androidcook <ian@darwinsys.com>
2013-07-10 08:25:44 -0400

    Put three examples up.

--- RangeGraphDemo/src/com/example/rangegraph/MainActivity.java
+++ RangeGraphDemo/src/com/example/rangegraph/MainActivity.java
@@ -9,6 +9,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final int min = 140, max = 160;
+		
+		RangeGraph low = (RangeGraph) findViewById(R.id.lowGauge);
+		low.setMin(min); low.setMax(max); low.setValue(138);
+		RangeGraph ok = (RangeGraph) findViewById(R.id.okGauge);
+		ok.setMin(min); ok.setMax(max); ok.setValue(146);
+		RangeGraph hi = (RangeGraph) findViewById(R.id.highGauge);
+		hi.setMin(min); hi.setMax(max); hi.setValue(162);
 	}
 
 }
