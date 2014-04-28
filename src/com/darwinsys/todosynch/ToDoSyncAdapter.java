d2af56e05509f713a616d3d1e702399109daf2bc
Ian Darwin <ian@darwinsys.com>
2014-04-27 21:17:36 -0400

    Add newer constructor.

--- src/com/darwinsys/todosynch/ToDoSyncAdapter.java
+++ src/com/darwinsys/todosynch/ToDoSyncAdapter.java
@@ -3,6 +3,7 @@ package com.darwinsys.todosynch;
 import android.accounts.Account;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SyncResult;
 import android.os.Bundle;
@@ -18,15 +19,34 @@ import android.util.Log;
 public class ToDoSyncAdapter extends AbstractThreadedSyncAdapter {
 	
 	private final static String TAG = "ReadingSyncAdapter";
+	
+	private final ContentResolver mContentResolver;
 
 	public ToDoSyncAdapter(Context appContext, boolean b) {
 		super(appContext, b);
 		Log.d(TAG, "ReadingSyncAdapter.ReadingSyncAdapter()");
+		mContentResolver = appContext.getContentResolver();
 	}
+	
+	/** Alternate constructor form to maintain compatibility with Android 3.0
+     * and later platform versions
+     */
+    public ToDoSyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+
+        mContentResolver = context.getContentResolver();
+    }
+
 
 	@Override
-	public void onPerformSync(Account account, Bundle extras, String authority,
-			ContentProviderClient provider, SyncResult syncResult) {
+	public void onPerformSync(Account account, 
+			Bundle extras, 
+			String authority,
+			ContentProviderClient provider, 
+			SyncResult syncResult) {
 		Log.d(TAG, "ReadingSyncAdapter.onPerformSync()");
 		
 		// TODO DO THE WORK HERE	
