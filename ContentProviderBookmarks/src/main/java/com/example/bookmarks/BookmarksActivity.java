2918afcd65535ebd47c95cd105e822d68909d5c5
Ian Darwin <ian@darwinsys.com>
2014-05-11 21:12:13 -0400

    Add a simple Bookmarks display from a ContentProvider.

---
+++ ContentProviderBookmarks/src/main/java/com/example/bookmarks/BookmarksActivity.java
@@ -0,0 +1,40 @@
+package com.example.bookmarks;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+
+public class BookmarksActivity extends ListActivity {
+
+    private static String TAG = BookmarksActivity.class.getSimpleName();
+    
+    // Herewith the standard "highly structured" parallel array anti-pattern enforced by Android
+    String[] from = { 
+    		Browser.BookmarkColumns.TITLE, 
+    		Browser.BookmarkColumns.URL,
+    };
+    int[] to = { 
+    		R.id.bmark_title, 
+    		R.id.bmark_url,
+    };
+    
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+        setContentView(R.layout.main);
+		Cursor qr = getContentResolver().query(Browser.BOOKMARKS_URI, null, null, null, null);
+		Log.d(TAG, "CURSOR rows = " + qr.getCount());
+		CursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_item, qr, from, to);
+		setListAdapter(adapter);
+    }
+
+}
+
