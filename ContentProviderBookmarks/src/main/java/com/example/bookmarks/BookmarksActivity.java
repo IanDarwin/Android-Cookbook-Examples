75c6eb0886d7fdcac874a271463a5150e08e4528
Ian Darwin <ian@darwinsys.com>
2014-05-13 21:07:34 -0400

    Half-baked Delete-on-long-press.

--- ContentProviderBookmarks/src/main/java/com/example/bookmarks/BookmarksActivity.java
+++ ContentProviderBookmarks/src/main/java/com/example/bookmarks/BookmarksActivity.java
@@ -5,8 +5,11 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Browser;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class BookmarksActivity extends ListActivity {
 
@@ -34,6 +37,20 @@ public class BookmarksActivity extends ListActivity {
 		Log.d(TAG, "CURSOR rows = " + qr.getCount());
 		CursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_item, qr, from, to);
 		setListAdapter(adapter);
+		getListView().setLongClickable(true);
+		getListView().setOnLongClickListener(new AdapterView.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				Log.d(TAG, "BookmarksActivity.getListView().onLongClick");
+				int ix = getListView().getSelectedItemPosition();
+				final int deleted = getContentResolver().delete(Browser.BOOKMARKS_URI, "_id = ?", new String[]{Integer.toString(ix)});
+				if (deleted == 1) {
+					Toast.makeText(BookmarksActivity.this, "Deleted!", Toast.LENGTH_SHORT).show();
+				}
+				return false;
+			}
+		});
     }
 
 }
