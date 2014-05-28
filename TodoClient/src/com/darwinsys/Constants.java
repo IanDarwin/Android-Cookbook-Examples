5e456fd9a9d52b4a9af5e085c704a119a86b9d00
Ian Darwin <ian@darwinsys.com>
2014-05-28 09:01:03 -0400

    Connectivity constants

---
+++ TodoClient/src/com/darwinsys/Constants.java
@@ -0,0 +1,10 @@
+package com.darwinsys;
+
+/** These are mostly things that should be in SharedPreferences */
+
+public class Constants {
+	public static final String SERVER = "172.17.162.115";
+	public static int PORT = 8080;
+	// CONTEXT may be the empty string or a directory name ending in "/"
+	public static final String CONTEXT = "todoserver/";
+}
