a1795d01fabf56121e8331bf4e7322b4b467d940
Ian Darwin <ian@darwinsys.com>
2014-05-29 10:03:23 -0400

    Paths

--- TodoClient/src/com/darwinsys/Constants.java
+++ TodoClient/src/com/darwinsys/Constants.java
@@ -5,6 +5,6 @@ package com.darwinsys;
 public class Constants {
 	public static final String SERVER = "172.17.162.115";
 	public static int PORT = 8080;
-	// PATH_PREFIX may be the empty string or a directory name ending in "/"
-	public static final String PATH_PREFIX = "rest/";
+	// PATH_PREFIX may be the empty string or a directory name
+	public static final String PATH_PREFIX = "rest";
 }
