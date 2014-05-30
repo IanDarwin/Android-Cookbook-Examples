eb4f3adcd416f6e56b699103341a47853f5afe9d
Ian Darwin <ian@darwinsys.com>
2014-05-30 09:59:01 -0400

    Imports police.

--- TodoModel/src/com/darwinsys/todo/model/Date.java
+++ TodoModel/src/com/darwinsys/todo/model/Date.java
@@ -1,11 +1,5 @@
 package com.darwinsys.todo.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.InputMismatchException;
 import java.util.Scanner;
