51321b91bad99b58a496adcdc9525a410b53f549
Ian Darwin <ian@darwinsys.com>
2015-10-16 13:08:27 -0400

    Start of an example using HttpClient for REST with basic-auth.

---
+++ RestDemo/src/main/java/com/examples/RestWithAuth.java
@@ -0,0 +1,22 @@
+public class MyActivity {
+
+	public void onSend(View v) {
+
+		...
+
+		DefaultHttpClient client = new DefaultHttpClient();
+
+		HttpPost req = new HttpPost(requestUri);
+		req.addHeader("content-type", "application/json");
+
+		// Base-64 encode username and password.
+		// DO NOT USE THIS unless you're sure the protocol is HTTPS
+		String userName = "SomeUser";
+		String password = "don't use this";
+		String authInfo = Base64.encodeToString(
+			(userName + ":" + password).getBytes(), 0);
+		request.addHeader("Authorization", authInfo);
+		
+		RequestHandler<String> handler = new BasicResponseHandler();
+		String result = client.execute(request, handler);
+
