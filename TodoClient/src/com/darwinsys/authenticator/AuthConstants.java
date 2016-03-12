f4263527729b777de29100ed06c667e0fc31adb7
Ian <ian@darwinsys.com>
2016-03-12 15:05:38 -0500

    Remove the unfinished Todo program here, in favor of TodoMore
    which is its own TLP and is (almost) finised to the point of usability.
    Well, it is usable either as a local client on Android or as web client
    (using JSF). The Sync Adapter is written but not fully debugged...

--- TodoClient/src/com/darwinsys/authenticator/AuthConstants.java
+++
@@ -1,7 +0,0 @@
-package com.darwinsys.authenticator;
-
-public class AuthConstants {
-
-	/** The account type's internal name. MUST agree with android:accountType in authenticator.xml! */
-	public static final String MY_ACCOUNT_TYPE = "com.darwinsys.accounts";
-}
--- TodoSyncServer/src/test/resources/import.sql
+++
@@ -1,6 +0,0 @@
--- Minimal data for testing.
--- ID/fkey values are chosen to avoid colliding with sequence-generated values
-
-insert into Task (id, name, createdYear, createdMonth, createdDay, project, context, complete, modified) values(-10, 'Get the lead out!', 2011,01,01, 'Plumbing', 'Home', false, 0);
-insert into Task (id, name, createdYear, createdMonth, createdDay, project, context, complete, modified) values(-11, 'Pay off loan', 2011,02,03, 'Accounting', 'Home', false, 0);
-insert into Task (id, name, createdYear, createdMonth, createdDay, project, context, complete, modified) values(-12, 'Call Home', 2011,03,03, null, 'Work', false, 0);
