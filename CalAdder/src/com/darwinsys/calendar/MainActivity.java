3584345ff98f2ff21730341b35868d39debfbfd2
Ian <ian@darwinsys.com>
2016-04-15 19:20:36 -0400

    Imports police.

--- CalAdder/src/com/darwinsys/calendar/MainActivity.java
+++ CalAdder/src/com/darwinsys/calendar/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
