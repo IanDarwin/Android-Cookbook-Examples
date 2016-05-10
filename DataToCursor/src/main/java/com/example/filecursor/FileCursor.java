b3884a245c44168fcc40c3f160f37bd3fa5b7f49
Ian <ian@darwinsys.com>
2016-05-10 12:04:18 -0400

    Rename project to DataToCursor. Start rearranging and renaming files too.

---
+++ DataToCursor/src/main/java/com/example/filecursor/FileCursor.java
@@ -0,0 +1,107 @@
+package com.example.filecursor;
+
+import android.database.AbstractCursor;
+import android.database.Cursor;
+
+/**
+ * Provide a Cursor from a fixed list of data
+ * column 1 - _id
+ * column 2 - filename
+ * column 3 - file type
+ */
+public class FileCursor extends AbstractCursor {
+
+	private static final String[] COLUMN_NAMES = {"_id", "filename", "type"};
+
+	private static final String[] DATA = {
+			"one.mpg",
+			"two.jpg",
+			"tre.dat",
+			"fou.git",
+	};
+
+	@Override
+	public int getCount() {
+		return DATA.length;
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return COLUMN_NAMES.length;
+	}
+
+	@Override
+	public String[] getColumnNames() {
+		return new String[]{"_id", "filename", "type"};
+	}
+
+	@Override
+	public int getType(int column) {
+		switch(column) {
+		case 0:
+			return Cursor.FIELD_TYPE_INTEGER;
+		case 1:
+		case 2:
+			return Cursor.FIELD_TYPE_STRING;
+		default: throw new IllegalArgumentException(Integer.toString(column));
+		}
+	}
+
+	/** 
+	 * Return the _id value.
+	 * Conveniently, rows and array indices are both 0-based.
+	 */
+	@Override
+	public int getInt(int column) {
+		int row = getPosition();
+		switch(column) {
+		case 0: return row;
+		default: throw new IllegalArgumentException(Integer.toString(column));
+		}
+	}
+
+	@Override
+	public String getString(int column) {
+		int row = getPosition();
+		switch(column) {
+		case 1: return DATA[row];
+		case 2: return extension(DATA[row]);
+		default: throw new IllegalArgumentException(Integer.toString(column));
+		}
+	}
+
+	/** Stub version, works for sample filenames like only */
+	private String extension(String path) {
+		return path.substring(4);
+	}
+
+	// Remaining get*() methods call this as there are no other column types
+	private char dieBadColumn() {
+		throw new IllegalArgumentException("No columns of this type");
+	}
+
+	@Override
+	public short getShort(int column) {
+		return (short) dieBadColumn();
+	}
+
+	@Override
+	public long getLong(int column) {
+		return dieBadColumn();
+	}
+
+	@Override
+	public float getFloat(int column) {
+		return dieBadColumn();
+	}
+
+	@Override
+	public double getDouble(int column) {
+		return dieBadColumn();
+	}
+
+	@Override
+	public boolean isNull(int column) {
+		return false;
+	}
+}
