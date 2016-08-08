178c9898762a0b479abc1212afaee96f17502842
Ian <ian@darwinsys.com>
2016-08-07 20:10:35 -0400

    Add a Card widget demo

---
+++ CardDemo/README.1line.txt
@@ -0,0 +1,1 @@
+Demonstation of Card widget.
---
+++ CardDemo/app/src/main/java/com/androidcookbook/carddemo/CardActivity.java
@@ -0,0 +1,30 @@
+package com.androidcookbook.carddemo;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+
+        // Dynamically set the image and text
+
+        ImageView img = (ImageView) findViewById(R.id.house_front_view);
+        Drawable d = ContextCompat.getDrawable(this,R.drawable.fixer_upper_1);
+        img.setImageDrawable(d);
+
+        TextView descr = (TextView) findViewById(R.id.house_descr);
+        descr.setText("Beautiful fixer-upper! Only used on weekends by respectable Hobbit couple!");
+
+    }
+}
