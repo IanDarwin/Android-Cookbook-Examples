b7526e97a294e59f2bae80f5abed1787fc5ae1cf
Ian <ian@darwinsys.com>
2016-08-08 19:03:11 -0400

    Renamed layout, clean imports

--- CardDemo/app/src/main/java/com/androidcookbook/carddemo/CardActivity.java
+++ CardDemo/app/src/main/java/com/androidcookbook/carddemo/CardActivity.java
@@ -4,18 +4,15 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class CardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card);
+        setContentView(R.layout.card_layout);
 
         // Dynamically set the image and text
 
