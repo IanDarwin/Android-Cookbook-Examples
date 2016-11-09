ae478c87dc44ab34c17d9cda1b665060548b2a42
Ian <ian@darwinsys.com>
2016-11-09 15:46:33 -0500

    Show both forms used in the Recipe, two buttons, api22 perms, xml onClick, Java 7, ...

--- EmailWithAttachments/src/com/example/emailandroid/Main.java
+++ EmailWithAttachments/src/com/example/emailandroid/Main.java
@@ -1,15 +1,25 @@
 package com.example.emailandroid;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class Main extends Activity implements OnClickListener {
+/**
+ *  Email With Attachments example
+ */
+public class Main extends Activity {
         private static final String TAG = "Main";
-        private Button emailButton;
+        private final static String EXTRA_RECIPIENT = "somebody@someplace.domain";
 
         /** Called when the activity is first created. */
         @Override
@@ -20,26 +30,50 @@ public class Main extends Activity implements OnClickListener {
                 // Set the View Layer
                 setContentView(R.layout.main);
 
-                // Get reference to Email Button
-                this.emailButton = (Button) this.findViewById(R.id.emailButton);
-
-                // Sets the Event Listener onClick
-                this.emailButton.setOnClickListener(this);
-
+                // The Event Listener onClicks are set in the XML
+                
             }
 
-        @Override
-        public void onClick(View view) {
-            if (view == this.emailButton) {
-                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-                emailIntent.setType("text/html");
-                emailIntent.putExtra(android.content.Intent.EXTRA_TITLE, "My Title");
-                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Subject");
+        public void oneAttachment(View view) {
+        	Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        	emailIntent.setType("text/html");
+        	emailIntent.putExtra(Intent.EXTRA_EMAIL, EXTRA_RECIPIENT);
+        	emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Subject");
 
-                // Obtain reference to (hard-coded) String and pass it to Intent
-                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, 
-                    getString(R.string.my_text));
-                startActivity(emailIntent);
-            }
+        	// Obtain reference to (hard-coded) String and pass it to Intent
+        	emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, 
+        			getString(R.string.my_text));
+        	startActivity(emailIntent);
         }
+        
+        public void twoAttachments(View v) {
+        	Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        	intent.setType("text/plain");
+        	intent.putExtra(Intent.EXTRA_SUBJECT, "Test multiple attachments");
+        	intent.putExtra(Intent.EXTRA_TEXT, "Mail with multiple attachments");
+        	intent.putExtra(Intent.EXTRA_EMAIL, new String[]{EXTRA_RECIPIENT});
+
+        	File baseDir = 
+        		Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        	baseDir.mkdirs();
+        	File f1 = new File(baseDir, "File1.txt");
+        	writeToFile(f1, "This is file 1 contents");
+        	File f2 = new File(baseDir, "File2.txt");
+        	writeToFile(f2, "This is file 2 contents");
+        	ArrayList<Uri> uris = new ArrayList<Uri>();
+        	uris.add(Uri.fromFile(f1));
+        	uris.add(Uri.fromFile(f2));
+
+        	intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+        	startActivity(intent);
+        }
+
+		private void writeToFile(File file, String contents) {
+			file.delete();
+			try (PrintWriter pout = new PrintWriter(file)) {
+				pout.println(contents);
+			} catch (IOException e) {
+				Toast.makeText(this,"Improbable IOException writing: " + e, Toast.LENGTH_LONG).show();
+			}
+		}
 }
