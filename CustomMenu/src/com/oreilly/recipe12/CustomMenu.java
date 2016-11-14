e7b5fc1ecf83a61e7e45257554750bea4e099033
Ian <ian@darwinsys.com>
2016-11-14 13:38:24 -0500

    Replace Activity to make this be the example from the book/website.

--- CustomMenu/src/com/oreilly/recipe12/CustomMenu.java
+++ CustomMenu/src/com/oreilly/recipe12/CustomMenu.java
@@ -3,17 +3,13 @@ package com.oreilly.recipe12;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SubMenu;
-import android.widget.Toast;
+import android.view.MenuInflater;
 
+/** Shows displaying a menu of our own choosing.
+ * Does not have action handling yet; see next recipe.
+ * @author Rachee Singh
+ */
 public class CustomMenu extends Activity {
-	
-    
-	private static final int OPTION_1 = 0;
-	private static final int OPTION_2 = 1;
-	private int GROUP_ID = 4;
-	private int ITEM_ID =3;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,28 +17,10 @@ public class CustomMenu extends Activity {
         setContentView(R.layout.main);
 
     }
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	
-        SubMenu sub1 = menu.addSubMenu(GROUP_ID, ITEM_ID , Menu.NONE, R.string.submenu_1);
-        sub1.setHeaderIcon(R.drawable.icon);
-        sub1.setIcon(R.drawable.icon);
-       
-        sub1.add(GROUP_ID , OPTION_1, 0, "Submenu Option 1");
-        sub1.add(GROUP_ID, OPTION_2, 1, "Submenu Option 2");
-       
-		return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case OPTION_1: 
-            	Toast.makeText(this, "Submenu 1, Option 1", Toast.LENGTH_LONG).show();
-                break;
-            case OPTION_2:
-            	Toast.makeText(this, "Submenu 1, Option 2", Toast.LENGTH_LONG).show();
-                break;
-        }
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 }
