1795e39d70817a4819f2198ef4ff66789f85bbe9
Ian <ian@darwinsys.com>
2016-11-15 21:36:42 -0500

    Slow down gravity; can now move left and right

--- SimpleJumper/src/com/androidcookbook/simplejumper/Droid.java
+++ SimpleJumper/src/com/androidcookbook/simplejumper/Droid.java
@@ -3,17 +3,15 @@ package com.androidcookbook.simplejumper;
 import org.flixel.FlxG;
 import org.flixel.FlxSound;
 import org.flixel.FlxSprite;
-import org.flixel.FlxState;
-import org.flixel.ui.FlxInputText;
 
 public class Droid extends FlxSprite {
     private final FlxSound sound = new FlxSound();
 
     public Droid(int X, int Y) {
         super(X, Y);
-        // loadGraphic(R.drawable.player, true, true);
+        // loadGraphic("player", true, true);
         maxVelocity.x = 100; // walking speed
-        acceleration.y = 400; // gravity
+        acceleration.y = 10; // gravity
         drag.x = maxVelocity.x * 4; // deceleration (sliding to a stop)
 
         // tweak the bounding box for better feel
@@ -31,36 +29,36 @@ public class Droid extends FlxSprite {
       }
 
     @Override
-    public void update() {
-        // Smooth slidey walking controls
-        acceleration.x = 0;
-        if (FlxG.keys.LEFT)
-        	acceleration.x -= drag.x;
-        if (FlxG.keys.RIGHT)
-        	acceleration.x += drag.x;
+	public void update() {
+		// Smooth slidey walking controls
+		acceleration.x = 0;
+		if (FlxG.keys.LEFT)
+			acceleration.x -= drag.x;
+		if (FlxG.keys.RIGHT)
+			acceleration.x += drag.x;
 
-        if (isTouching(FLOOR)) {
-            // Jump controls
-            if (FlxG.keys.UP) {
-                // sound.loadEmbedded(R.raw.jump);
-                // sound.play();
+		if (isTouching(FLOOR)) {
+			// Jump controls
+			if (FlxG.keys.UP) {
+				// sound.loadEmbedded(R.raw.jump);
+				// sound.play();
 
-                velocity.y = -acceleration.y * 0.51f;
-                play("jump");
+				velocity.y = -acceleration.y * 0.51f;
+				play("jump");
 
-              }// Animations
-            else if (velocity.x > 0) {
-                play("walk");
-              }
-            else if (velocity.x < 0) {
-                play("walk_back");
-              }
-            else play("idle");
-          }
-        else if (velocity.y < 0) play("jump");
-        else play("flail");
+			} // Animations
+			else if (velocity.x > 0) {
+				play("walk");
+			} else if (velocity.x < 0) {
+				play("walk_back");
+			} else
+				play("idle");
+		} else if (velocity.y < 0)
+			play("jump");
+		else
+			play("flail");
 
         // Default object physics update
-        super.update();
+        //super.update();
       }
 }
