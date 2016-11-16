afc1d12fcb80361d75351b5028853cbf4a1d91c6
Ian <ian@darwinsys.com>
2016-11-16 08:59:51 -0500

    Reformat code a bit

--- SimpleJumper/src/com/androidcookbook/simplejumper/PlayState.java
+++ SimpleJumper/src/com/androidcookbook/simplejumper/PlayState.java
@@ -3,11 +3,9 @@ package com.androidcookbook.simplejumper;
 import org.flixel.FlxState;
 import org.flixel.FlxText;
 
-public class PlayState extends FlxState
-{
+public class PlayState extends FlxState {
 	@Override
-	public void create()
-	{
+	public void create() {
 		add(new FlxText(0, 0, 200, "SimpleJumper 0.0"));
 		add(new Droid(50, 50));
 	}
