d59c9a69c4ea78ef842a99b8c8659a174aec7a17
Ian Darwin <ian@darwinsys.com>
2022-03-31 15:32:58 -0400

    Start of demo of AsyncTask

---
+++ AsyncTaskDemo/app/src/main/java/com/example/asynctaskdemo/DemoTask.java
@@ -0,0 +1,62 @@
+package com.example.asynctaskdemo;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * This class is the actual task, started from the main.
+ * Type params are Input, Progress, Result
+ */
+public class DemoTask extends AsyncTask<String, Integer, String> {
+
+    final static String TAG = DemoTask.class.getSimpleName();
+
+    final MainActivity main;
+
+    DemoTask(MainActivity main) {
+        this.main = main;
+    }
+
+    /**
+     *  Called when the client invokes execute() on the task.
+     */
+    @Override
+    protected void onPreExecute() {
+        Log.d(TAG, "onPreExecute");
+        super.onPreExecute();
+    }
+
+    /** Called in the background thread
+     * @param strings The inputs, from execute(). Only [0] is used.
+     */
+    @Override
+    protected String doInBackground(String... strings) {
+        Log.d(TAG, "doInBackground: " + strings[0]);
+        for (int i = 10; i > 0; i--) {
+            try {
+                publishProgress(Integer.valueOf(i));
+                Thread.sleep(1000);
+            } catch (InterruptedException cantHappen) {
+                cantHappen.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /** Called on the UI thread in response to publishProgress() above.
+     *
+     * @param values The String that was passed into publishProgress().
+     */
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        Log.d(TAG, "onProgressUpdate");
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        Log.d(TAG, "onPostExecute");
+        super.onPostExecute(s);
+    }
+
+}
