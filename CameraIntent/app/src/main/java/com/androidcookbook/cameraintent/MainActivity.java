35d775d77020462bbce7b9981da7327a10ac0368
Ian Darwin <ian@darwinsys.com>
2022-07-26 14:11:35 -0400

    Finish rewrite using new API, FileProvider, etc.

--- CameraIntent/app/src/main/java/com/androidcookbook/cameraintent/MainActivity.java
+++ CameraIntent/app/src/main/java/com/androidcookbook/cameraintent/MainActivity.java
@@ -10,13 +10,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import java.io.File;
 
-public class MainActivity extends Activity {
+/** This simple demo shows getting results back from another Activity
+ * (in this case, in another app) using the API 30-era API
+ */
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "CameraLaunchingActivity";
     private final static int ACTION_TAKE_PICTURE = 123;
 
     private File pictureFile;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +33,37 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    // Must register this before the Activity is started, so just make it a field.
+    final ActivityResultLauncher<Uri> mGetContent = registerForActivityResult(new ActivityResultContracts.TakePicture(),
+            success -> {
+                // Handle the returned Uri
+                if (success) {
+                    final String message = getString(R.string.picture_saved) + " " + pictureFile.length();
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                    Log.d(TAG, message);
+                } else {
+                    Log.d(TAG, "Picture taking failed");
+                }
+            }
+    );
+
     public void takePicture(View v) {
         Log.d(TAG, "Starting Camera Activity");
         try {
             // Use an Intent to get the Camera app going.
             Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            imageIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
             // Set up file to save image into.
             File baseDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            File pictureFile = new File(baseDir, "picture1234.jpg");
-            imageIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
-            imageIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                    Uri.fromFile(pictureFile));
+            pictureFile = new File(baseDir, "picture1234.jpg");
+            uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", pictureFile);
             // And away we go!
-            // startActivityForResult(imageIntent, ACTION_TAKE_PICTURE);
-			ActivityResultLauncher<String> mGetContent = registerForActivityResult(new GetContent(),
-				uri -> {
-					// Handle the returned Uri
-					final String message = getString(R.string.picture_saved) + " " + uri;
-					Log.d(TAG, message);
-					Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-				}
-			);
-			mGetContent.launch();
+			mGetContent.launch(uri);
+
         } catch (Exception e) {
-            Toast.makeText(this,
-                    getString(R.string.cant_start_activity) + ": " + e,
-                    Toast.LENGTH_LONG).show();
+            final String mesg = getString(R.string.cant_start_activity) + ": " + e;
+            Toast.makeText(this, mesg, Toast.LENGTH_LONG).show();
+            Log.d(TAG, mesg);
         }
     }
 }
